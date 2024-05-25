9e59773818eff2cd6c158ebe46a48f9c3593b89c
abhig1909 <98597920+abhig1909@users.noreply.github.com>
2024-05-26 02:09:28 +0530

    day 2

--- day2.py
+++ day2.py
@@ -1,3 +1,72 @@
-q=1
-w=2
-print(q+w)
+#Arithemic Operator.
+
+print('Addition',1+2)
+print('sub', 2-1)
+print('multi',3*4)
+print('divison',4/2)
+print('rem', 5%3)
+print('floor division',7//2)
+print('expometial', 4**4)
+
+
+num1=12
+num2=5
+
+# Arithmetic operations
+
+total=num1+num2
+subs=num1-num2
+product=num1*num2
+division=num1/num2
+reminder=num1%num2
+
+# Printing values with label
+
+
+print('total: ', total)
+print('subs: ',subs)
+print('product: ',  product)
+print('divissions: ', division)
+print('reminder: ', reminder)
+
+
+
+#alculate (area, volume,density, weight, perimeter, distance, force).
+
+#Area of circle.
+
+radius=10
+pie=3.14
+
+area_of_circle=(pie*radius*radius)
+print('the area of circle is = :' ,area_of_circle)
+
+
+
+#area of ractangle.
+
+length= int(input('enter the value'))
+bredth= int(input('enter the value'))
+
+Area =length*bredth
+
+print('the area of ractangle is :', Area)
+
+
+
+#weight of body.
+
+gravity=9.18
+mass=70
+
+weight= mass*gravity
+
+print('the weight of the body is =' ,weight )
+
+
+volume=0.078
+mass=76
+
+density= mass/volume
+
+print('the density of liquid is =', density)
