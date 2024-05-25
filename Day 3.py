a8627144b833b8e1b83982cd83339583036e2fc9
abhig1909 <98597920+abhig1909@users.noreply.github.com>
2024-05-26 04:14:29 +0530

    New day 3

---
+++ Day 3.py
@@ -0,0 +1,38 @@
+#Comparison Operators
+
+print(3>2)
+print(3>=2)
+print (3<2)
+print(2<=3)
+print(3==3)
+print(2!=1)
+
+print(len('Abhishek') == len('super'))
+
+print(len('Abhishek') != len('super'))
+
+print(len('Abhishek') < len('super'))
+
+print(len('Abhishek') > len('super'))
+
+# Comparing something gives either a True or False
+
+x= True
+y= False
+
+print('true == true:' , True == True)
+
+print('true == false:' , True == False)
+
+print('false == false:' , False == False)
+
+
+
+print ('1 is 1' , 1 is 1 )
+print('1 is not 2' , 1 is not 2)
+
+print('A in abhishek' , 'a' in 'abhishek')
+
+print('sh in abhishek' , 'sh' in 'abhishek')
+
+print( 'data ' in 'abhishek is data analyst' )
